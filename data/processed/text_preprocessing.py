dfa605cc392cf0c2510ce2d190b09b7c545fc001
Suraj Subrahmanyan <ssubrahmanyan11@gmail.com>
2025-08-20 20:32:16 -0400

    Complete text preprocessing pipeline

--- data/processed/text_preprocessing.py
+++ data/processed/text_preprocessing.py
@@ -1,28 +1,58 @@
 import jsonlines
 import nltk
 import spacy
+import contractions
+import string
+
+#nltk.download("stopwords")
 
 # 1. make lowercase 2. expand contractions 3. remove punctuation 4. tokenize 5. lemmatize 6. remove stopwords
-#write functions to do manually instead of libraries
-#deal with possessive nouns like occam's using nltk's POS (part of speech) tagging
 
-#nltk.download("punkt_tab")
+from nltk.corpus import stopwords
 
-from nltk.tokenize import word_tokenize
+def removePunctuation(text):
+    punctuationString = string.punctuation
+    #Replace with space so that Hello-World --> Hello World
+    translation_table = str.maketrans(punctuationString, " " * len(punctuationString), "") 
+    text = text.translate(translation_table)
+    #For Hello: World --> Hello  World, we can remove all whitespace and then add one space between each
+    text = ' '.join(text.split())
+    return text
 
-with jsonlines.open('processed_papers_final.jsonl', 'r') as reader:
-    line_count = 0
-    for lines in reader:
-        s = lines["title"]
-        s = s.replace(':', '')
-        s = s.replace('-', '')  
-        s = s.replace("'s", '')     
-        stemming = spacy.load("en_core_web_sm")
-        s = stemming(s)
-        print(s)
-        if (line_count >= 10):
-            break
-        line_count += 1
+def removeStopwords(text):
+    noStopwords = []
+    stopwords_list = stopwords.words("english") #ex. something because
+    split_string = text.split() #list ["something", "because"]
+
+    for words in split_string: #for each word "something", "because", etc. that is not stopword, add to list
+        if words not in stopwords_list:
+            noStopwords.append(words)
+
+    text = " ".join(noStopwords) #join them back to be tokenized with spacy
+    return text    
+
+def lemmatizer(text):
+    lemmatized_list = []
+    model = spacy.load("en_core_web_sm")
+    doc = model(text)
+    for tokens in doc:
+        lemmatized_list.append(tokens.lemma_)
+    return lemmatized_list
 
 if __name__ == '__main__':
-    pass
+    with jsonlines.open('processed_papers_final.jsonl', 'r') as reader:
+        line_count = 0
+        for lines in reader:
+            s = lines["title"]
+            s = s.lower() #ex. Bob --> bob
+            s = contractions.fix(s) #ex. it's --> it is
+            s = removePunctuation(s) #ex. Hello: World --> Hello World
+            s = removeStopwords(s)
+            s = lemmatizer(s)
+            print(s)
+            
+
+            #Testing
+            # if (line_count >= 200):
+            #     break
+            # line_count += 1
