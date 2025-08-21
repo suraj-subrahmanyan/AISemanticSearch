fc74f65843fb3003691c4b9d0cfe75c1c4f75390
Suraj Subrahmanyan <ssubrahmanyan11@gmail.com>
2025-08-21 12:37:05 -0400

    Include ID for each paper and revise file writing

--- data/processed/text_preprocessing.py
+++ data/processed/text_preprocessing.py
@@ -8,6 +8,8 @@ import string
 
 # 1. make lowercase 2. expand contractions 3. remove punctuation 4. tokenize 5. lemmatize 6. remove stopwords
 
+model = spacy.load("en_core_web_sm")
+
 from nltk.corpus import stopwords
 
 def removePunctuation(text):
@@ -33,44 +35,41 @@ def removeStopwords(text):
 
 def lemmatizer(text):
     lemmatized_list = []
-    model = spacy.load("en_core_web_sm")
     doc = model(text)
     for tokens in doc:
         lemmatized_list.append(tokens.lemma_)
     return lemmatized_list
 
 if __name__ == '__main__':
-    final_nlp = []
     with jsonlines.open('processed_papers_final.jsonl', 'r') as reader:
-        # line_count = 0
-        for lines in reader:
-            temp_dict = {}
+        with jsonlines.open('nlp_text.jsonl', 'w') as writer:
+            # line_count = 0
+            for lines in reader:
+                temp_dict = {}
 
-            #Titles
-            t_nlp = lines['title']
-            t_nlp = t_nlp.lower() #ex. Bob --> bob
-            t_nlp = contractions.fix(t_nlp) #ex. it's --> it is
-            t_nlp = removePunctuation(t_nlp) #ex. Hello: World --> Hello World
-            t_nlp = removeStopwords(t_nlp)
-            temp_dict['title'] = lemmatizer(t_nlp)
+                #id
+                temp_dict['id'] = lines['id']
 
-            #Summaries
-            s_nlp = lines['summary']
-            s_nlp = s_nlp.lower()
-            s_nlp = contractions.fix(s_nlp)
-            s_nlp = removePunctuation(s_nlp)
-            s_nlp = removeStopwords(s_nlp)
-            temp_dict['summary'] = lemmatizer(s_nlp)
+                #Titles
+                t_nlp = lines['title']
+                t_nlp = t_nlp.lower() #ex. Bob --> bob
+                t_nlp = contractions.fix(t_nlp) #ex. it's --> it is
+                t_nlp = removePunctuation(t_nlp) #ex. Hello: World --> Hello World
+                t_nlp = removeStopwords(t_nlp)
+                temp_dict['title'] = lemmatizer(t_nlp)
 
-            final_nlp.append(temp_dict)
+                #Summaries
+                s_nlp = lines['summary']
+                s_nlp = s_nlp.lower()
+                s_nlp = contractions.fix(s_nlp)
+                s_nlp = removePunctuation(s_nlp)
+                s_nlp = removeStopwords(s_nlp)
+                temp_dict['summary'] = lemmatizer(s_nlp)
 
-            #Testing
-            # if (line_count >= 50):
-            #     break
-            # line_count += 1
-    
-    print(final_nlp)
-                
-    with jsonlines.open('nlp_text.jsonl', 'w') as writer:
-        writer.write_all(final_nlp)
-                
+                writer.write(temp_dict)
+                print("line complete")
+
+                #Testing
+                # if (line_count >= 50):
+                #     break
+                # line_count += 1                
